5b405d82b48f75b340b3572550056c46ce7ff4c9
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-19 23:19:14 -0500

    able to receive object with axios.get

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios'
+
+const databaseurl = `https://swapi.dev/api/people/`
+
 
 const App = () => {
+
+  axios.get(databaseurl).then(data=>{
+
+    console.log(data.data)}).catch(error=>{console.log(error)})
+  
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
