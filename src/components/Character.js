8e177ca61c7c9da4c8ecd30c5e4fc3ff545c4099
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-20 15:30:10 -0500

    added character name inside buttons and added a reduced opacity around character wrapper background

--- src/components/Character.js
+++ src/components/Character.js
@@ -5,8 +5,10 @@ import styled from "styled-components"
 
 const Wrapper = styled.div`
 width: 80%;
-border: 1px solid green;
+border: 1px solid white;
 margin: 0 auto;
+background: rgba(239, 207, 237, .5);
+
 `
 
 const Button = styled.button`
@@ -14,16 +16,16 @@ width: 200px;
 height 100px;
 color: yellow;
 background-color: black;
-margin: 0 auto;`
+margin: 0 auto;
+opacity:1;
+`
 
 
 export default function Character(props){
     
     return (
-    
     <Wrapper>
-    <h1>{props.name}</h1>
-    <Button>Click</Button>
+    <Button>{props.name}</Button>
     </Wrapper>
     )
 }
