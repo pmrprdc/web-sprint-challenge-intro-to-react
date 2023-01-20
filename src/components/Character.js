31969d9a82d933819cb24c335362ec5dd850f914
Pamir Perdeci <pamirperdeci@gmail.com>
2023-01-20 15:24:30 -0500

    styled a button and a wrapper for the character component using styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,7 +1,29 @@
 // Write your Character component here
 import React from "react";
+import styled from "styled-components"
+
+
+const Wrapper = styled.div`
+width: 80%;
+border: 1px solid green;
+margin: 0 auto;
+`
+
+const Button = styled.button`
+width: 200px;
+height 100px;
+color: yellow;
+background-color: black;
+margin: 0 auto;`
+
 
 export default function Character(props){
     
-    return <h1>{props.name}</h1>
+    return (
+    
+    <Wrapper>
+    <h1>{props.name}</h1>
+    <Button>Click</Button>
+    </Wrapper>
+    )
 }
